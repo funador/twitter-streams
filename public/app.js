5284f0d8aee8e5eb9255969f39acb1bbdc3064c9
Jesse <jesse.heaslip@gmail.com>
2016-04-06 11:45:04 -0500

    removing delete functionality to test performance

--- public/app.js
+++ public/app.js
@@ -3,12 +3,12 @@
 angular.module('app', ["firebase"])
 
 .controller("storyCtrl", function($scope, $firebaseArray, $timeout) {
-  $scope.sports = ['nfl', 'nba', 'mlb', 'nhl']
 
-  let ref = new Firebase("https://twitter-streams.firebaseio.com")
+  var ref = new Firebase("https://twitter-streams.firebaseio.com")
   $scope.nfl      = $firebaseArray(ref.child("nfl").orderByChild("count").limitToLast(30))
   $scope.mlb      = $firebaseArray(ref.child("mlb").orderByChild("count").limitToLast(30))
   $scope.nba      = $firebaseArray(ref.child("nba").orderByChild("count").limitToLast(30))
+  $scope.nhl      = $firebaseArray(ref.child("nba").orderByChild("count").limitToLast(30))
 
   // ref.remove()
 
