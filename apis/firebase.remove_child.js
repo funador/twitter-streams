3eb7c1f8be3b9f99a6c28ba2c1c43d91f2c5d283
Jesse <jesse.heaslip@gmail.com>
2016-04-08 20:34:34 -0500

    updating removal script

--- apis/firebase.remove_child.js
+++ apis/firebase.remove_child.js
@@ -1,18 +1,15 @@
 'use strict'
 
-// there is a problem here....
 module.exports = {
   remove_child: (ref) => {
     var cutoff = Date.now() - 1 * 60 * 60 * 1000
 
     ref.child('all/unfluff').orderByChild("timestamp").endAt(cutoff).limitToLast(1).on('child_added', (snap) => {
-      // I think here you need to loop through and delete
-      // check what comes back in the snap?
 
       var tweet = snap.val()
       var id = snap.key()
 
-      console.log("story deleted")
+      console.log("--------------------------------", tweet)
 
       ref.child(`all/unfluff/${id}`).remove()
       ref.child(`all/imagesize/${id}`).remove()
