f60aa0c85ca86080a995c222068dd02e0978e94e
Jesse <jesse.heaslip@gmail.com>
2016-04-08 20:24:18 -0500

    upping removal time

--- apis/firebase.remove_child.js
+++ apis/firebase.remove_child.js
@@ -3,7 +3,7 @@
 // there is a problem here....
 module.exports = {
   remove_child: (ref) => {
-    var cutoff = Date.now() - 1 * 15 * 60 * 1000
+    var cutoff = Date.now() - 1 * 60 * 60 * 1000
 
     ref.child('all/unfluff').orderByChild("timestamp").endAt(cutoff).limitToLast(1).on('child_added', (snap) => {
       // I think here you need to loop through and delete
@@ -12,7 +12,7 @@ module.exports = {
       var tweet = snap.val()
       var id = snap.key()
 
-      console.log("story deleted")  
+      console.log("story deleted")
 
       ref.child(`all/unfluff/${id}`).remove()
       ref.child(`all/imagesize/${id}`).remove()
