fff61e483a59f6d4b0861cc5afc2b8a9be468185
Jesse <jesse.heaslip@gmail.com>
2016-04-19 15:31:04 -0500

    adding success callback to delete

--- apis/firebase.remove_child.js
+++ apis/firebase.remove_child.js
@@ -11,12 +11,20 @@ module.exports = {
         var tweet = snap.val()
         var id = snap.key()
 
+        var onComplete = function(err) {
+          if (err) {
+            console.log('Synchronization failed');
+          } else {
+            console.log('Synchronization succeeded');
+          }
+        };
+
         console.log("-----------deleting tweet: ", `${tweet.topic}`)
 
         ref.child(`all/unfluff/${id}`).remove()
         ref.child(`all/imagesize/${id}`).remove()
         ref.child(`all/shorten/${id}`).remove()
-        ref.child(`${tweet.topic}/${id}`).remove()
+        ref.child(`${tweet.topic}/${id}`).remove(onComplete)
       })
     }, 5000)
 
