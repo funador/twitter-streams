70bd8784845a874e57a784460f8f4633768e3ffc
Jesse <jesse.heaslip@gmail.com>
2016-04-21 00:28:16 -0500

    bumping up time on deletes

--- apis/firebase.remove_child.js
+++ apis/firebase.remove_child.js
@@ -6,7 +6,7 @@ module.exports = {
   remove_child: (ref) => {
 
     setInterval(function () {
-      var cutoff = Date.now() - 1 * 60 * 60 * 1000
+      var cutoff = Date.now() - 4 * 60 * 60 * 1000
 
       // do a transacton?
       ref.child('all/unfluff').orderByChild("timestamp").endAt(cutoff).limitToLast(1).once('value', (snap) => {
@@ -14,13 +14,20 @@ module.exports = {
           var tweet = snap.val()
           var id = Object.keys(snap.val())[0]
 
+          var onComplete = function(err) {
+            if (err) {
+              console.log("DELETE FAILED--------:", tweet[id].id)
+            } else {
+              console.log("deleting--------:", tweet[id].topic)
+            }
+          };
+
+          ref.child(`all/unfluff/${id}`).remove()
           ref.child(`all/imagesize/${id}`).remove()
           ref.child(`all/shorten/${id}`).remove()
-          ref.child(`${tweet.topic}/${id}`).remove()
-          ref.child(`all/unfluff/${id}`).remove()
-          console.log("deleting--------:", tweet[id].topic)
-        }
+          ref.child(`${tweet.topic}/${id}`).remove(onComplete)
 
+        }
       })
     }, 100)
 
