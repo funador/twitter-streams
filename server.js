b6c30246e7ebb15e7b2474dc3e861575f4e02d5f
funador <jesse.heaslip@gmail.com>
2017-09-26 00:24:17 -0500

    updating naming of delete stories file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { startStream } = require('./lib/start-stream')
-const { deleteOldStories } = require('./lib/delete-old-stories')
+const { deleteStaleStories } = require('./lib/delete-stale-stories')
 const { deleteStaleSocial } = require('./lib/delete-stale-social')
 const { trackSentiment } = require('./lib/track-sentiment')
 const { clearKloutCue } = require('./lib/clear-klout-cue')
@@ -11,15 +11,15 @@ const { mailer } = require('./lib/mailer')
 
 
 startStream()
-// deleteOldStories()
-deleteStaleSocial('klouters')
-deleteStaleSocial('twitters')
+// deleteStaleStories()
+deleteStaleSocial('klouters', 5)
+deleteStaleSocial('twitters', 1)
 trackSentiment()
 // clearKloutCue()
 // trackSothbeys()
 process.on('unhandledRejection', r => console.log(r));
 
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV == 'production') {
   process.on('unhandledRejection', r => {
     mailer(r)
   });
