28df7cff68254a2ab7f5ebd1d6a655f9d9e15200
funador <jesse.heaslip@gmail.com>
2017-09-30 12:22:55 -0500

    lastest work on algo

---
+++ cheking.js
@@ -0,0 +1,10 @@
+[ 'accountAge', 'convos', 'default_profile_image', 'description', 'favourites_count', 
+'followers_count', 'frequentLen', 'friends_count', 'has_extended_profile', 'lang', 'langMatch', 'likes', 'listed_count', 
+'mins', 'numAts', 'numAtsDesc', 'numHash', 'numHashDesc', 'numOriginalTweets', 'numsInName', 
+'quotedStatus', 'retweets', 'sources', 'statuses_count', 'totalFreq', 'url', 'verified', 'withoutLinks' ]
+
+
+[ 'accountAge', 'mins', 'frequentLen', 'totalFreq', 'gender', 'convos', 'listed_count', 'numOriginalTweets', 
+'sources', 'retweets', 'likes', 'withoutLinks', 'numHash', 'numAts', 'quotedStatus', 'langMatch', 
+'numAtsDesc', 'numHashDesc', 'description', 'numsInName', 'followers_count', 'friends_count', 'statuses_count', 
+'lang', 'verified', 'favourites_count', 'url', 'has_extended_profile', 'default_profile_image' ]
--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const { trackSentiment } = require('./lib/track-sentiment')
 const { clearKloutCue } = require('./lib/clear-klout-cue')
 const { mailer } = require('./lib/mailer')
 // require('./lib/test')
-// const { trackSothbeys } = require('./lib/track-sothbeys')
-
 
 startStream()
 // deleteStaleStories()
@@ -16,11 +14,12 @@ deleteStaleSocial('klouters', 5)
 deleteStaleSocial('twitters', 1)
 trackSentiment()
 // clearKloutCue()
-// trackSothbeys()
-process.on('unhandledRejection', r => console.log(r));
 
 if(process.env.NODE_ENV == 'production') {
   process.on('unhandledRejection', r => {
     mailer(r)
   });
+}
+else {
+  process.on('unhandledRejection', r => console.log(r));  
 }
