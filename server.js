d6844f74368f07dd033163508163bf43d49768b0
funador <jesse.heaslip@gmail.com>
2018-02-12 11:48:09 -0600

    reverting to sported

--- server.js
+++ server.js
@@ -3,18 +3,9 @@ require('dotenv').config()
 const { startStream } = require('./lib/start-stream')
 const { deleteStaleStories } = require('./lib/cleanup/delete-stale-stories')
 const { deleteStaleSocial } = require('./lib/cleanup/delete-stale-social')
-const { clearKloutCue } = require('./lib/cleanup/clear-klout-cue')
 const { mailer } = require('./lib/mailer')
 
-// Tools to build data
-// require('./lib/algo/algo-seed-data')
-// require('./lib/algo/ham-maker')
-
-startStream('stream')
-// deleteStaleStories()
-deleteStaleSocial('klouters', 5)
-deleteStaleSocial('twitters', 1)
-// clearKloutCue()
+startStream()
 
 if(process.env.NODE_ENV == 'production') {
   process.on('unhandledRejection', r => {
