64201948d1f5b39506cff4a9819062926c744147
funador <jesse.heaslip@gmail.com>
2018-02-14 16:36:38 -0600

    adding express to connect to port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+require('dotenv').config({path: __dirname + '/.env', silent: true})
 
+const app = require('express')()
 const startStream = require('./lib/start-stream')
 const deleteStaleStories = require('./lib/cleanup/delete-stale-stories')
-const mailer = require('./lib/mailer')
 
 startStream()
 deleteStaleStories()
+
+app.listen(process.env.PORT || 3000, () => console.log('listening'))
