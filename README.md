b258a61da42308ac8553d2d7e934acac05116435
funador <jesse.heaslip@gmail.com>
2018-02-13 23:56:29 -0600

    updating comments

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Back-end to pull and analyze content from Twitter using social cues
 
-To use you will need API keys for Twitter, Firebase and Klout
+To use you will need API keys for Twitter and Firebase
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const startStream = require('./lib/start-stream')
 const deleteStaleStories = require('./lib/cleanup/delete-stale-stories')
-const { mailer } = require('./lib/mailer')
+const mailer = require('./lib/mailer')
 
 startStream()
 deleteStaleStories()
