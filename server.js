0f784fd5812adb9e53e4ff7614b4934a32463463
funador <jesse.heaslip@gmail.com>
2018-07-10 17:00:44 -0500

    removing memcheck and cleaning up delete in fullExtract

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 require('dotenv').config()
 
-const memwatch = require('memwatch-next')
-memwatch.on('leak', info => {
-  console.error('Memory leak detected:\n', info)
-});
-
 const app = require('express')()
 require('./lib/start-stream')()
 require('./lib/cleanup/delete-stale-stories')()
