64b8209b78ce04483f03f7e545f34f0d036e9cec
funador <jesse.heaslip@gmail.com>
2019-01-26 23:03:43 -0600

    adding recent work

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = require('express')()
 require('./lib/start-stream')()
 require('./lib/cleanup/delete-stale-stories')()
 
-app.listen(process.env.PORT || 8081, () => console.log('listening'))
+app.listen(process.env.PORT || 8081, () => console.log('👍'))
